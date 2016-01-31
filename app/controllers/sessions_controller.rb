6be7f8027f6373f41c0086f74bad24d6e3fbde98
Aram Zadikian <brancusi@gmail.com>
2016-01-31 06:24:49 -0800

    wip

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,8 @@ class SessionsController < Devise::SessionsController
       token: self.resource.authentication_token,
       first_name: self.resource.first_name,
       last_name: self.resource.last_name,
-      email: self.resource.email
+      email: self.resource.email,
+      extra: 'test'
     }
 
     render json: data, status: 201
