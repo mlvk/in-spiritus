eb1bd8a1f475c0d10c4de99c92df6e52c7fa6a63
Vinh Luu <vinhlt3@gmail.com>
2016-08-23 05:40:53 +0700

    IS-9 Hard code start_time, end_time, drop_off_count, pick_up_count on route_plan resource (#13)

--- app/resources/route_plan_resource.rb
+++ app/resources/route_plan_resource.rb
@@ -1,6 +1,10 @@
 class RoutePlanResource < JSONAPI::Resource
   attributes :date,
-             :published_state
+             :published_state,
+             :start_time,
+             :end_time,
+             :drop_off_count,
+             :pick_up_count
 
   has_one    :user
   has_many   :route_visits
@@ -9,6 +13,22 @@ class RoutePlanResource < JSONAPI::Resource
              :user,
              :published_state,
              :id
+
+  def start_time
+    360
+  end
+
+  def end_time
+    660
+  end
+
+  def drop_off_count
+    20
+  end
+
+  def pick_up_count
+    10
+  end
 
   paginator  :offset
 end
