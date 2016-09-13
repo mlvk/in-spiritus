34257ebf6bcae27e685d0ce2352c2b3f62168e46
Vinh Luu <vinhlt3@gmail.com>
2016-09-13 10:16:22 +0700

    Vinhlt3/ch253/unit test location previous stock level (#53)
    * add unit test location previous stock level
    
    * add unit test for getting location previous stock level

---
+++ test/factories/stock_levels_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :stock_level do
+    starting 3
+    returns 1
+  end
+end
--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -51,4 +51,48 @@ class LocationTest < ActiveSupport::TestCase
     location2 = create(:location)
     assert_equal(location2.code, location2.code.downcase)
   end
+
+  test "returns stock level when get previous stock level" do
+    create_list(:item, 2, :product)
+    location = create(:location)
+
+    today = Date.today
+    3.times do |index|
+      date = today - index
+      stock = create(:stock, location: location, taken_at: date.to_time)
+
+      Item.all.each do |item|
+        create(:stock_level, stock: stock, item: item)
+      end
+    end
+
+    today_stock_level = Stock
+      .where(:location_id => location.id)
+      .where(:taken_at => today)
+      .first
+      .stock_levels
+      .first
+
+    yesterday_stock_level = Stock
+      .where(:location_id => location.id)
+      .where(:taken_at => (today - 1))
+      .first
+      .stock_levels
+      .first
+
+    assert_equal(yesterday_stock_level,
+      location.previous_stock_level(today_stock_level),
+      "match previous stock level of today stock level")
+
+    before_yesterday_stock_level = Stock
+      .where(:location_id => location.id)
+      .where(:taken_at => (today - 2))
+      .first
+      .stock_levels
+      .first
+
+    assert_equal(nil,
+      location.previous_stock_level(before_yesterday_stock_level),
+      "match previous stock level of the day before yesterday stock level")
+  end
 end
